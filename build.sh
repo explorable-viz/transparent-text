df7e08f0dd1d53a360de14bc369bd5baadd385a2
JosephBond <jecb1998@googlemail.com>
2024-08-27 14:57:05 +0100

    ACM reference format being added building skeleton of project structure

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+PDFLATEX="pdflatex -file-line-error -halt-on-error"
+TARGET=${1:-main}
+
+$PDFLATEX $TARGET
+bibtex $TARGET
+$PDFLATEX $TARGET
+$PDFLATEX $TARGET
+rm -f $TARGET.aux $TARGET.dvi $TARGET.log $TARGET.bbl $TARGET.blg $TARGET.out $TARGET.pag $TARGET.cb $TARGET.cb2 $TARGET.toc
