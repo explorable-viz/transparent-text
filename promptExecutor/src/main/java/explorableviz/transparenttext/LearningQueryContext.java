20d009d3e1343d0463390c61b9568ce5327ad205
Alfonso <alfonso.piscitelli91@gmail.com>
2025-01-24 10:49:43 +0000

    [update] minors. newline and fixed path for learning cases

--- promptExecutor/src/main/java/explorableviz/transparenttext/LearningQueryContext.java
+++ promptExecutor/src/main/java/explorableviz/transparenttext/LearningQueryContext.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class LearningQueryContext  {
-
+    private static final String LEARNING_CASE_PATH = "learningCases";
     private String systemPrompt;
 
     private ArrayList<QueryContext> learningCasePaths;
@@ -25,11 +25,10 @@ public class LearningQueryContext  {
     public static LearningQueryContext importLearningCaseFromJSON(String jsonLearningCasePath, int numCasesToGenerate) throws IOException {
         JSONObject jsonLearningCase = new JSONObject(new String(Files.readAllBytes(Path.of(jsonLearningCasePath))));
         String systemPrompt = jsonLearningCase.getString("system_prompt");
-        String folderLearningCases = jsonLearningCase.getString("learningCasesPath");
         ArrayList<QueryContext> learningCases = new ArrayList<>();
         Random random = new Random(0);
 
-        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(folderLearningCases), Files::isRegularFile);
+        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(LEARNING_CASE_PATH), Files::isRegularFile);
         for (Path filePath : directoryStream) {
             String content = new String(Files.readAllBytes(filePath));
             JSONObject jsonTestCase = new JSONObject(content);
