b30e75b5686051786ab13c920d0286ed0ca498dd
rolyp <rolyp@users.noreply.github.com>
2025-02-04 14:22:19 +0000

    deploy: d90d8acbb697b5a3d4c13cca9f9e5961c5f2fba7

--- test.mjs
+++ test.mjs
@@ -1,6 +1,7 @@
-import { runTests, testURL, waitForFigure } from "./shared/webtest-lib.js";
+import { runTests, testURL, waitForFigure, waitFor } from "./shared/webtest-lib.js";
 
 export const main = async () => {
     await runTests(testURL("figure-spm4b")([ (page) => waitForFigure(page)("fig-leftBarChart") ]))();
+    await runTests(testURL("table-spm1")([ (page) => waitFor("text#fig-explanation119")(page) ]))();
     console.log("Success!");
 };
