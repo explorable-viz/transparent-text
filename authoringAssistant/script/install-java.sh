2a990a0fd58c3228ca42e59774f59e5cc95c03b1
Alfonso <alfonso.piscitelli91@gmail.com>
2025-02-07 12:06:41 +0000

    [update] install_java.sh always tested in the wf

--- authoringAssistant/script/install-java.sh
+++ authoringAssistant/script/install-java.sh
@@ -4,19 +4,18 @@ set -xe
 # Function to install Java
 install_java() {
     echo "Java is not installed. Installation attempt..."
-    if command -v apt &> /dev/null; then
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        brew update
+        brew install openjdk
+        # Symbolic link for macos.
+        sudo ln -sfn "$(brew --prefix openjdk)/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk.jdk
+    elif command -v apt &> /dev/null; then
         # Debian/Ubuntu based
         sudo apt update
         sudo apt install -y default-jre default-jdk
     elif command -v yum &> /dev/null; then
         # CentOS/RHEL based
         sudo yum install -y java-11-openjdk
-    elif command -v brew &> /dev/null; then
-        # macOS based
-        brew update
-        brew install openjdk
-        # Symbolic link for macos.
-        sudo ln -sfn "$(brew --prefix openjdk)/libexec/openjdk.jdk" /Library/Java/JavaVirtualMachines/openjdk.jdk
     else
         echo "Error: neither apt nor yum found. Java installation failed."
         exit 1
@@ -28,9 +27,7 @@ install_java() {
         exit 1
     fi
     echo "Java successfully installed."
+    java --version
 }
 
-# Check if Java is installed
-if ! command -v java &> /dev/null; then
-    echo install_java
-fi
+install_java
