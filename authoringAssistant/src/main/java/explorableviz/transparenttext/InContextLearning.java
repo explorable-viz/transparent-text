6632de1ffa280cdb2c4a624e9b2e6a16e9b20537
Alfonso <alfonso.piscitelli91@gmail.com>
2025-03-06 16:19:07 +0000

    [new] new structure of system-prompt

--- authoringAssistant/src/main/java/explorableviz/transparenttext/InContextLearning.java
+++ authoringAssistant/src/main/java/explorableviz/transparenttext/InContextLearning.java
@@ -1,9 +1,17 @@
 package explorableviz.transparenttext;
+
 import it.unisa.cluelab.lllm.llm.prompt.PromptList;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static explorableviz.transparenttext.Query.loadQuery;
 
 public class InContextLearning {
@@ -17,9 +25,8 @@ public class InContextLearning {
     }
 
     public static InContextLearning importLearningCaseFromJSON(String jsonLearningCasePath, int numCasesToGenerate) throws Exception {
-        JSONObject jsonLearningCase = new JSONObject(new String(Files.readAllBytes(Path.of(jsonLearningCasePath))));
         ArrayList<Query> learningCases = loadQuery(Settings.getLearningCaseFolder(), numCasesToGenerate);
-        return new InContextLearning(jsonLearningCase.getString("system_prompt"), learningCases);
+        return new InContextLearning(loadSystemPrompt(jsonLearningCasePath), learningCases);
     }
 
     public PromptList toPromptList() throws Exception {
@@ -29,6 +36,23 @@ public class InContextLearning {
             inContextLearning.addPairPrompt(query.toUserPrompt(), query.getExpected());
         }
         return inContextLearning;
+    }
+
+    public static String loadSystemPrompt(String directoryPath) throws IOException {
+        Path systemPromptPath = Paths.get(directoryPath, "system-prompt.txt");
+        String systemPrompt = Files.exists(systemPromptPath) ? STR."\{Files.readString(systemPromptPath)}\n" : "";
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            return systemPrompt + paths.filter(Files::isRegularFile)
+                    .filter(path -> !path.getFileName().toString().equals("system-prompt.txt"))
+                    .map(path -> {
+                        try {
+                            return Files.readString(path);
+                        } catch (IOException e) {
+                            throw new RuntimeException("Error reading file: " + path, e);
+                        }
+                    })
+                    .collect(Collectors.joining("\n"));
+        }
     }
 
     public int size() {
