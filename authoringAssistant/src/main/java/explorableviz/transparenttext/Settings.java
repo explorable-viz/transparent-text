1599e55726a1d3c0165b61dc344848853e558d8a
Alfonso <alfonso.piscitelli91@gmail.com>
2025-03-06 10:12:04 +0000

    [update] manage type in json

--- authoringAssistant/src/main/java/explorableviz/transparenttext/Settings.java
+++ authoringAssistant/src/main/java/explorableviz/transparenttext/Settings.java
@@ -5,46 +5,93 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 public class Settings {
 
     private static Settings instance;
-    private JSONObject settings;
-    public static Settings getInstance() {
-        if(instance == null) instance = new Settings();
+    private final JSONObject settings;
+
+    private static Settings getInstance() {
+        if (instance == null) throw new AssertionError("You have to call init first");
         return instance;
     }
 
-    public void loadSettings(String settingsPath) throws IOException {
-        File f = new File(settingsPath);
-        Path p = Paths.get(f.toURI());
-        String content_settings = new String(Files.readAllBytes(p));
-        this.settings = new JSONObject(content_settings);
+    public static Settings init(String settingsPath) throws IOException {
+        instance = new Settings(settingsPath);
+        return instance;
     }
 
-    private String get(String key) {
-        return this.settings.getString(key);
+    private Settings(String settingsPath) throws IOException {
+        this.settings = new JSONObject(new String(Files.readAllBytes(Paths.get(new File(settingsPath).toURI()))));
     }
 
-    public JSONObject getSettings() {
-        return settings;
+//    private String get(String key) {
+//        return this.settings.getString(key);
+//    }
+
+    public static JSONObject getSettings() {
+        return getInstance().settings;
     }
 
-    public int getLimit() {
-        return Integer.parseInt(this.get("agent-limit"));
+    public static int getLimit() {
+        return getSettings().getInt("agent-limit");
     }
 
-    public String getFluidTempFolder() {
-        return this.get("fluid-temp-folder");
+    public static String getFluidTempFolder() {
+        return getSettings().getString("fluid-temp-folder");
     }
 
-    public String getLibrariesBasePath() {
-        return this.get("base-path-library");
-    }
-    public String getFluidCommonFolder() {
-        return this.get("fluid-common-folder");
+    public static String getLibrariesBasePath() {
+        return getSettings().getString("base-path-library");
     }
 
+    public static String getFluidCommonFolder() {
+        return getSettings().getString("fluid-common-folder");
+    }
+
+    public static float getTemperature() {
+        return getSettings().getFloat("temperature");
+    }
+
+    public static int getNumContextToken() {
+        return getSettings().getInt("num_ctx");
+    }
+
+    public static String getLogFolder() {
+        return getSettings().getString("log-folder");
+    }
+
+    public static boolean isReasoningEnabled() {
+        return getSettings().getBoolean("enable-reasoning");
+    }
+
+    public static String getLearningCaseFolder() {
+        return getSettings().getString("learning-case-folder");
+    }
+
+    public static String getTestCaseFolder() {
+        return getSettings().getString("test-case-folder");
+    }
+
+    public static float getThreshold() {
+        return getSettings().getFloat("threshold");
+    }
+
+    public static String getSystemPromptPath() {
+        return getSettings().getString("system-prompt-path");
+    }
+
+    public static int getNumTestToGenerate() {
+        return getSettings().getInt("num-test-to-generate");
+    }
+
+    public static int getNumLearningCaseToGenerate() {
+        return getSettings().getInt("num-learning-case-to-generate");
+    }
+
+    public static Optional<Integer> getNumQueryToExecute() {
+        return (getSettings().has("num-query-to-execute") && !getSettings().isNull("num-query-to-execute")) ? Optional.of(getSettings().getInt("num-query-to-execute")) : Optional.empty();
+    }
 }
